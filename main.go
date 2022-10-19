8aa005d721ccdfd0d17d75717fde991b8e5fa89e
unknown <primaptr27@gmail.com>
2022-10-19 12:27:12 +0700

    upload file baru

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+
+	authcontroller "github.com/jeypc/go-auth/controllers"
+)
+
+var temp *template.Template
+
+func init() {
+	temp = template.Must(template.ParseGlob("views/*.html"))
+}
+func RunIndex(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "index.html", nil)
+}
+func main() {
+	fs := http.FileServer(http.Dir("assets"))
+	http.Handle("/assets/", http.StripPrefix("/assets", fs))
+	http.HandleFunc("/", authcontroller.Index)
+	http.HandleFunc("/dashboard", authcontroller.Dashboard)
+	http.HandleFunc("/permission", authcontroller.Permission)
+	http.HandleFunc("/calendar", authcontroller.Calendar)
+	http.HandleFunc("/login", authcontroller.Login)
+	http.HandleFunc("/logout", authcontroller.Logout)
+	http.HandleFunc("/register", authcontroller.Register)
+	http.HandleFunc("/adminlogbook", authcontroller.LogbookAdmin)
+
+	fmt.Println("Server jalan di: http://localhost:3000")
+	http.ListenAndServe(":3000", nil)
+}
